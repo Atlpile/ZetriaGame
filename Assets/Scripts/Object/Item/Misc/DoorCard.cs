000f830d800e79e922462a9b364f5211d797fdef
LAPTOP-CI3C75GC\Atlplle <2276021874@qq.com>
2023-07-20 21:19:17 +0800

    为使用SO备份

--- Assets/Scripts/Object/Item/Misc/DoorCard.cs
+++ Assets/Scripts/Object/Item/Misc/DoorCard.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DoorCard : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed = 3f;
+    private Vector3 _direction;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name == "Player")
@@ -12,5 +15,11 @@ public class DoorCard : MonoBehaviour
             GameManager.Instance.AudioManager.AudioPlay(E_AudioType.Effect, "collect");
             Destroy(this.gameObject);
         }
+    }
+
+    private void Update()
+    {
+        _direction = new Vector2(0, Mathf.Sin(_moveSpeed * Time.deltaTime));
+        transform.Translate(_direction);
     }
 }
